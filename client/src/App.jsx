2940127438aeafcb9120ef115fd969d16ca9aabd
PrestonNguyen2001 <prestonnguyen2001@gmail.com>
2024-06-08 11:59:08 -0400

    Created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import Dashboard from "./pages/Dashboard"
+import Portfolio from "./pages/Portfolio"
+import Contact from "./pages/Contact"
+import Resume from "./pages/Resume"
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">Hello world!</h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
