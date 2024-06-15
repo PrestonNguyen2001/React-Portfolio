57faf847b2d71145e0c22766f0251a9e9becaef5
PrestonNguyen2001 <prestonnguyen2001@gmail.com>
2024-06-15 12:19:36 -0400

    Update footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,16 +1,19 @@
 import { Footer } from "flowbite-react";
 import { Link } from "react-router-dom";
 import { BsFacebook, BsInstagram, BsTwitter, BsGithub } from "react-icons/bs";
+
 export default function FooterCom() {
   return (
-    <Footer container className="border border-t-8 border-teal-500">
+    <Footer
+      container
+      className="border-t-2 bg-light-background dark:bg-dark-background text-light-text dark:text-dark-text"
+    >
       <div className="w-full max-w-7xl mx-auto">
         <div className="grid w-full justify-between sm:flex md:grid-cols-1">
           <div className="mt-5">
             <Link
               to="/"
-              className="self-center whitespace-nowrap text-lg sm:text-xl 
-                font-semibold dark:text-white"
+              className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold"
             >
               <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
                 Preston&apos;s
@@ -20,24 +23,32 @@ export default function FooterCom() {
           </div>
           <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
             <div>
-              <Footer.Title title="about" />
+              <Footer.Title
+                title="about"
+                className="text-light-text dark:text-dark-text"
+              />
               <Footer.LinkGroup col>
                 <Footer.Link
                   href="/about"
                   target="_blank"
                   rel="noopener noreferrer"
+                  className="text-light-text dark:text-dark-text"
                 >
                   Preston&apos;s Portfolio
                 </Footer.Link>
               </Footer.LinkGroup>
             </div>
             <div>
-              <Footer.Title title="Follow Us" />
+              <Footer.Title
+                title="Follow Us"
+                className="text-light-text dark:text-dark-text"
+              />
               <Footer.LinkGroup col>
                 <Footer.Link
                   href="https://github.com/PrestonNguyen2001"
                   target="_blank"
                   rel="noopener noreferrer"
+                  className="text-light-text dark:text-dark-text"
                 >
                   Github Account
                 </Footer.Link>
@@ -45,17 +56,36 @@ export default function FooterCom() {
                   href="linkedin.com/in/preston-nguyen-8a08a52a8"
                   target="_blank"
                   rel="noopener noreferrer"
+                  className="text-light-text dark:text-dark-text"
                 >
                   LinkedIn
                 </Footer.Link>
-                <Footer.Link href="#">Instagram</Footer.Link>
+                <Footer.Link
+                  href="#"
+                  className="text-light-text dark:text-dark-text"
+                >
+                  Instagram
+                </Footer.Link>
               </Footer.LinkGroup>
             </div>
             <div>
-              <Footer.Title title="Legal" />
+              <Footer.Title
+                title="Legal"
+                className="text-light-text dark:text-dark-text"
+              />
               <Footer.LinkGroup col>
-                <Footer.Link href="#">Privacy Policy</Footer.Link>
-                <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
+                <Footer.Link
+                  href="#"
+                  className="text-light-text dark:text-dark-text"
+                >
+                  Privacy Policy
+                </Footer.Link>
+                <Footer.Link
+                  href="#"
+                  className="text-light-text dark:text-dark-text"
+                >
+                  Terms &amp; Conditions
+                </Footer.Link>
               </Footer.LinkGroup>
             </div>
           </div>
@@ -66,14 +96,28 @@ export default function FooterCom() {
             href="#"
             by="Preston's Blog"
             year={new Date().getFullYear()}
+            className="text-light-text dark:text-dark-text"
           />
           <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
-            <Footer.Icon href="#" icon={BsFacebook} />
-            <Footer.Icon href="#" icon={BsInstagram} />
-            <Footer.Icon href="#" icon={BsTwitter} />
+            <Footer.Icon
+              href="#"
+              icon={BsFacebook}
+              className="text-light-text dark:text-dark-text"
+            />
+            <Footer.Icon
+              href="#"
+              icon={BsInstagram}
+              className="text-light-text dark:text-dark-text"
+            />
+            <Footer.Icon
+              href="#"
+              icon={BsTwitter}
+              className="text-light-text dark:text-dark-text"
+            />
             <Footer.Icon
               href="https://github.com/PrestonNguyen2001"
               icon={BsGithub}
+              className="text-light-text dark:text-dark-text"
             />
           </div>
         </div>
