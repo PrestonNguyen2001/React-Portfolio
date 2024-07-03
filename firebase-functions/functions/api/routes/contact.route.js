a81fbf9418c0e63d920d45ad724dfc16adb80e6e
PrestonNguyen2001 <prestonnguyen2001@gmail.com>
2024-07-03 14:01:39 -0400

    Removed firebase functions

--- firebase-functions/functions/api/routes/contact.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { createContact } = require("../controllers/contact.controller.js");
-
-const router = express.Router();
-
-router.post("/create", createContact);
-
-module.exports = router;
--- firebase-functions/functions/index.js
+++
@@ -1,4 +0,0 @@
-const functions = require("firebase-functions");
-const app = require("./api/index.js");
-
-exports.api = functions.https.onRequest(app);
