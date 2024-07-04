3d0b8d361c0666de3d8aa3fbea539463afc446f9
PrestonNguyen2001 <prestonnguyen2001@gmail.com>
2024-07-04 12:01:40 -0400

    Fix build scripts

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -4,6 +4,7 @@ import { errorHandler } from "./error.js";
 export const verifyToken = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
   console.log("Token from headers:", token);
+
   if (!token) {
     console.log("No token found");
     return next(errorHandler(401, "You are not authenticated"));
@@ -11,7 +12,7 @@ export const verifyToken = (req, res, next) => {
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
-       console.log("Invalid token:", err.message);
+      console.log("Invalid token:", err.message);
       return next(errorHandler(403, "Token is not valid"));
     }
     console.log("Token verified, user:", user);
